0bcb3f5afcc24163a27dfec68e31512827338976
Cezar <nechiforr@uni.coventry.ac.uk>
2024-05-17 15:09:18 +0100

    Before OOV TOKEN PROPERTY

--- TensorFlow_ZeroToHero/Tokenization/tokenization.py
+++ TensorFlow_ZeroToHero/Tokenization/tokenization.py
@@ -5,9 +5,17 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 sentences = [
     'I love my dog',
     'I love my cat'
+    'You love my dog!',
+    'Do you think my dog is amazing?'
 ]
 
 tokenizer = Tokenizer(num_words = 100)
-tokenizer.fit_on_texts(sentences)
-word_index = tokenizer.word_index
-print(word_index)
+
+tokenizer.fit_on_texts(sentences) # SET OF SENTENCES TO TRAIN NEURAL NETWORK
+
+word_index = tokenizer.word_index 
+
+sequences = tokenizer.texts_to_sequences(sentences) # TOKENIZER GETS WORD INDEX AND CREATES SEQUENCES
+
+print(word_index, '\n') 
+print(sequences)
