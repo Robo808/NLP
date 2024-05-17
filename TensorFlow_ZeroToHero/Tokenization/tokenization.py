6e93da4505d4c7afbc1e15a5d6ac6f3865e4e411
Cezar <nechiforr@uni.coventry.ac.uk>
2024-05-17 17:46:23 +0100

    Tokenizer with Data

--- TensorFlow_ZeroToHero/Tokenization/tokenization.py
+++ TensorFlow_ZeroToHero/Tokenization/tokenization.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 sentences = [
     'I love my dog',
@@ -9,7 +10,7 @@ sentences = [
     'Do you think my dog is amazing?'
 ]
 
-tokenizer = Tokenizer(num_words = 100)
+tokenizer = Tokenizer(num_words = 100, oov_token="<OOV>") # OOV = Out of Vocabulary
 
 tokenizer.fit_on_texts(sentences) # SET OF SENTENCES TO TRAIN NEURAL NETWORK
 
@@ -19,3 +20,15 @@ sequences = tokenizer.texts_to_sequences(sentences) # TOKENIZER GETS WORD INDEX 
 
 print(word_index, '\n') 
 print(sequences)
+
+with open("sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
